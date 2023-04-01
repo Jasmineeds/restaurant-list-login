7c6c12a241609b103f499e84b81c541e29563614
jasmine <jasmine12122005@yahoo.com.tw>
2023-04-01 18:55:31 +0800

    feat: set basic routes

--- app.js
+++ app.js
@@ -1,40 +1,57 @@
 // app.js
 // require packages used in the project
 const express = require('express')
+const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const Restaurant = require('./models/restaurant')
+
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = express()
 const port = 3000
-const exphbs = require('express-handlebars')
-const restaurantList = require('./restaurant.json').results
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // connect to mongoDB
+
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-app.use(express.static("public"))
+app.use(express.static('public'))
+// request monitored by bodyparser
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-  res.render('index', { restaurant: restaurantList })
+  Restaurant.find() // take data from Restaurant model
+    .lean() // transform objects in Mongoose Model to JavaScript data
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.error(error))
 })
 
 // search bar
 app.get('/search', (req, res) => {
-
   const keywords = req.query.keywords
-  const keyword = keywords.trim().toLowerCase()
-
-  const filterData = restaurantList.filter(
-    data =>
-      data.name.toLowerCase().includes(keyword) ||
-      data.category.includes(keyword)
-  )
-
-  res.render("index", { restaurant: filterData, keywords })
+  Restaurant.find({ name: { $regex: keywords, $options: 'i' } })
+    .lean()
+    .then(restaurants => res.render('index', { restaurants, keywords }))
+    .catch(error => console.error(error))
 })
-
 
 // render each restaurant content
 app.get('/restaurants/:id', (req, res) => {
-  const restaurant = restaurantList.find(restaurant => restaurant.id.toString() === req.params.id)
-  res.render('show', { restaurant: restaurant })
+  Restaurant.findOne({ id: req.params.id })
+    .lean()
+    .then(restaurant => res.render('show', { restaurant }))
+    .catch(error => console.error(error))
 })
 
 // start and listen on the Express server
