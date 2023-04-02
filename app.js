74f69b7ad8ace4de6e62f8f94455b89d36536836
jasmine <jasmine12122005@yahoo.com.tw>
2023-04-02 16:25:14 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,25 +1,12 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require("./routes")
-
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+require("./config/mongoose")
 
 const app = express()
 const port = 3000
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // connect to mongoDB
-
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.engine('hbs', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'hbs')
