a33607e6da24b17c822f96b6a2c8047d40ceaabb
jasmine <jasmine12122005@yahoo.com.tw>
2023-05-01 01:16:25 +0800

    feat: add seeder to create users and restaurants

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,7 +1,7 @@
 const db = require('../../config/mongoose')
 const Restaurant = require('../restaurant')
 const User = require('../user')
-const SEED_RESTAURANT = require('../restaurant.json') // restaurant raw data in json
+const SEED_RESTAURANT = require('../restaurant.json').results // restaurant raw data in json
 const SEED_USER = require('../user.json') // user raw data in json
 const bcrypt = require('bcryptjs')
 
@@ -11,29 +11,38 @@ if (process.env.NODE_ENV !== 'production') {
 
 db.once('open', () => {
   console.log('running restaurantSeeder script...')
+
   // gen seed user info in db
-  Promise.all(
-    SEED_USER.map(user => {
-      return bcrypt
-        .genSalt(10)
-        .then(salt => bcrypt.hash(user.password, salt))
-        .then(hash => User.create({
+  const createUsers = SEED_USER.map(user => {
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(user.password, salt))
+      .then(hash =>
+        User.create({
           name: user.name,
           email: user.email,
           password: hash
-        }))
-        // gen seed restaurant data in db
-        .then(user => {
-          const userId = user._id
-          for (i = 0; i < SEED_USER.length; i++) {
-            SEED_USER[i].userRestaurants.map(item => {
-              SEED_RESTAURANT[item - 1].userId = userId
-              Restaurant.create(SEED_RESTAURANT[item - 1])
-            })
-          }
         })
+      )
+  })
+
+  // gen seed user's restaurant data in db
+  Promise.all(createUsers)
+    .then(users => {
+      const createRestaurants = []
+      users.forEach((user, index) => {
+        const { _id } = user
+        SEED_USER[index].userRestaurants.forEach(item => {
+          const restaurant = SEED_RESTAURANT[item - 1]
+          restaurant.userId = _id
+          createRestaurants.push(Restaurant.create(restaurant));
+        })
+      })
+      return Promise.all(createRestaurants);
     })
-  )
+    .catch(error => {
+      console.error(error)
+    })
     .then(() => {
       console.log('done.')
       process.exit()
